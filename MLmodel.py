320d828c124c2e7267b2c7d543b6073b72730f2e
Achraf-Trabelsi <achref.trabelsi@insat.ucar.tn>
2022-02-09 20:14:10 +0100

    third commit

--- MLmodel.py
+++ MLmodel.py
@@ -2,7 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-data=pd.read_csv("Ames_Housing_Sales.csv")
+#understanding data
+data=pd.read_csv("AmesHousing.csv")
 print(data.describe())
 print(data.columns)
-print(data.isnull().sum())
+print(data.isnull().sum().to_string())
+print(data.info())
+#cleaning data
