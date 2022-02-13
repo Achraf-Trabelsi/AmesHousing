be42211a5a391768fad79cf9adfba7011e660e82
Achraf-Trabelsi <achref.trabelsi@insat.ucar.tn>
2022-02-13 19:09:02 +0100

    sixth commit

--- MLmodel.py
+++ MLmodel.py
@@ -1,8 +1,10 @@
+from cProfile import label
 import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.impute import SimpleImputer
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 #understanding data
 data=pd.read_csv("AmesHousing.csv")
@@ -23,5 +25,11 @@ data=data.drop(labels='Fireplace Qu',axis=1)
 data['Garage Type'].fillna(data['Garage Type'].mode()[0], inplace = True)
 data['Garage Finish'].fillna(data['Garage Finish'].mode()[0], inplace = True)
 data['Garage Qual'].fillna(data['Garage Qual'].mode()[0], inplace = True)
-data['Garage Cond'].fillna(data['Garage Qual'].mode()[0], inplace = True)
+data['Garage Cond'].fillna(data['Garage Cond'].mode()[0], inplace = True)
+data['Lot Frontage'].fillna(data['Lot Frontage'].mode()[0], inplace = True)
 print(data.isnull().sum().to_string())
+
+#train test 
+target=data.SalePrice
+d=data.drop(labels='SalePrice',axis=1)
+dtrain,dtest,ptrain,ptest=train_test_split(d,target,train_size=0.8,random_state=True)
