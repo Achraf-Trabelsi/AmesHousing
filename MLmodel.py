8f75637626d41cfa329ecda6b37eea0fa5ff1a42
Achraf-Trabelsi <achref.trabelsi@insat.ucar.tn>
2022-02-15 22:47:01 +0100

    7th commit

--- MLmodel.py
+++ MLmodel.py
@@ -1,33 +1,49 @@
-from cProfile import label
-import pandas as pd 
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.impute import SimpleImputer
 import numpy as np
 from sklearn.model_selection import train_test_split
 
-#understanding data
 data=pd.read_csv("AmesHousing.csv")
-#print(data.describe())
+#understanding data
+
 print(data.shape)
 print(data.columns)
 missing_values=data.isnull().sum().to_string()
+print(missing_values)
+col_missing=[col for col in data.columns if data[col].isnull().sum()>0 ]
+print(col_missing)
 
-#print(data.info())
 #cleaning data
-#droping 
-data=data.drop(labels='Pool QC',axis=1)
-data=data.drop(labels='Fence',axis=1)
-data=data.drop(labels='Misc Feature',axis=1)
-data=data.drop(labels='Alley',axis=1)
-data=data.drop(labels='Fireplace Qu',axis=1)
-#imputing
-data['Garage Type'].fillna(data['Garage Type'].mode()[0], inplace = True)
-data['Garage Finish'].fillna(data['Garage Finish'].mode()[0], inplace = True)
-data['Garage Qual'].fillna(data['Garage Qual'].mode()[0], inplace = True)
-data['Garage Cond'].fillna(data['Garage Cond'].mode()[0], inplace = True)
-data['Lot Frontage'].fillna(data['Lot Frontage'].mode()[0], inplace = True)
-print(data.isnull().sum().to_string())
+
+#dropping unuseful data (Total already gives as the info)
+data=data.drop(labels=['PID'],axis=1)
+data=data.drop(labels=['BsmtFin SF 2', 'Bsmt Unf SF','BsmtFin SF 1'],axis=1)
+
+#droping because number missing is so high
+high_missing_rate=['Pool QC','Fence','Misc Feature','Alley','Fireplace Qu']
+data=data.drop(labels=high_missing_rate,axis=1)
+
+#imputing for categorical features
+cat_fea=['Garage Type','Garage Finish','Garage Qual',
+'Garage Cond','Lot Frontage','Bsmt Qual', 'Bsmt Cond', 
+'Bsmt Exposure','BsmtFin Type 1', 'BsmtFin Type 2',
+'Mas Vnr Type','Electrical']
+for fea in cat_fea:
+    data[fea].fillna(data[fea].mode()[0],inplace=True)
+#imputing for numerical features
+num_fea=['Bsmt Full Bath','Bsmt Half Bath','Total Bsmt SF']
+for fea in num_fea:
+    data[fea].fillna(data[fea].mean(),inplace=True)
+
+data['Garage Yr Blt'].fillna(value=data['Garage Yr Blt'].median(),inplace=True)
+data['Garage Cars'].fillna(value=0,inplace=True)
+data['Garage Area'].fillna(value=0,inplace=True)
+data['Mas Vnr Area'].fillna(value=data['Mas Vnr Area'].median(),inplace=True)
+#visualization
+corrMatrix = data.corr()
+sns.heatmap(corrMatrix, annot=True)
+plt.show()
 
 #train test 
 target=data.SalePrice
